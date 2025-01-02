1513aa19b6e55a28ecd9dc059d736b21dd2cd9bc
DanielBarLev <danielbarlev00@gmail.com>
2025-01-02 14:53:29 +0200

    add file name path to config.py

--- config/config.py
+++ config/config.py
@@ -1,10 +1,15 @@
 import os
 
 class Config:
+    # Folders:
     RECORDS_PATH = os.path.join(os.getcwd(), "dataset", "records")
     AUDIO_PATH = os.path.join(os.getcwd(), "dataset", "processed_data", "audio")
     MEL_PATH = os.path.join(os.getcwd(), "dataset", "processed_data", "mel_spectrogram")
-
+    # Files:
+    AUDIO_FILE_PATH = os.path.join(AUDIO_PATH, "audio.pt")
+    MEL_PATH_CR = os.path.join(AUDIO_PATH, "class_repr")
+    MEL_PATH_TS = os.path.join(AUDIO_PATH, "training_set")
+    MEL_PATH_ES = os.path.join(AUDIO_PATH, "evaluation_set")
     # Audio parameters
     SAMPLE_RATE = 16_000
     MAX_LENGTH = 1 * SAMPLE_RATE
