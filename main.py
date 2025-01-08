c22950ea3e11bcf8c7e84d30911e1f63773abea1
DanielBarLev <danielbarlev00@gmail.com>
2025-01-08 22:41:00 +0200

    add main function that preforms section 1, 2 and 3 from assignment2 (read data, compute mel, printing dtw and confusion matrices...)

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
-from matplotlib import pyplot as plt
+from src.DTW import *
+from src.preparation import prepare_audio, prepare_mel_spectrogram, preprocess_tensors
 
-from classes.DTW import DTW
-from classes.CTC import CTC
-from classes.MelSpectrogram import MelSpectrogram
-from config.config import Config
-from src.preparation import prepare_audio, prepare_mel_spectrogram
-import torch
-from scipy.io.wavfile import read, write
-import numpy as np
-from scipy.signal import resample
-from scipy.signal import spectrogram
-import librosa
 
-if __name__ == "__main__":
-
-    print(f'Using: {Config.DEVICE}')
+def main():
+    # 1. Data Acquisition:
     class_repr, training_set, evaluation_set = prepare_audio(update=True)
 
+    # 2. Mel Spectrogram:
     class_repr_ms, training_set_ms, evaluation_set_ms = prepare_mel_spectrogram(class_repr['audio'],
                                                                                 training_set['audio'],
                                                                                 evaluation_set['audio'],
                                                                                 update=True)
+
+    class_repr_ms, training_set_ms, evaluation_set_ms = preprocess_tensors(class_repr_ms=class_repr_ms,
+                                                                           training_set_ms=training_set_ms,
+                                                                           evaluation_set_ms=evaluation_set_ms)
+
+    # 3. Dynamic time warping
+    # 3.a Select the class representative recordings as the reference database: class_repr_ms
+
+    # 3.b Implement the DTW algorithm that was described in the lecture.
+    # 3.c Compare each audio recording in the training set with each of the audios in the DB using DTW algorithm.
+    # 3.d Construct a distance matrix:
+    dtw_dist_matrix_train = compute_distance_matrix_dtw(class_repr_ms, training_set_ms)
+
+    # 3.e Show the distance matrix:
+    display_distance_matrix(dtw_dist_matrix_train)
+
+    # 3.f Set Threshold and Determine Classification:
+    threshold = find_optimal_threshold(dtw_dist_matrix_train)
+    print("Optimal Threshold:", threshold)
+
+    # Calculate the accuracy over the training set.
+    pred_labels_train = classify_with_threshold(dtw_dist_matrix_train, threshold)
+    train_accuracy = compute_accuracy(pred_labels_train)
+    print("Training Set Accuracy:", train_accuracy)
+
+    # 3.g Apply Threshold on Validation Set:
+    dtw_dist_matrix_eval = compute_distance_matrix_dtw(class_repr_ms, evaluation_set_ms)
+
+    pred_labels_eval = classify_with_threshold(dtw_dist_matrix_eval, threshold)
+    eval_accuracy = compute_accuracy(pred_labels_eval)
+    print("Validation Set Accuracy:", eval_accuracy)
+
+    # 3.h Plot the confusion matrix:
+    confusion_matrix = confusion_matrix_10x10(pred_labels_eval)
+    plot_confusion_matrix(confusion_matrix, title='Confusion Matrix (Validation Set)')
+
+if __name__ == '__main__':
+    main()
 
     # ctc = CTC()
     # print(ctc.pred)
@@ -31,48 +59,5 @@ if __name__ == "__main__":
     # print(prob)
     # print(seq)
     # print(mat)
-
-    # Initialize DTW
-    dtw = DTW(class_repr_ms, training_set_ms)
-
-    # Compute DTW distance matrix
-    dtw_matrix = dtw.compute_distance_matrix()
-    expected_result = torch.FloatTensor(range(10))
-    print(dtw_matrix.shape)
-
-    result=torch.argmin(dtw_matrix[0], dim=1)
-    print(dtw_matrix[0])
-    print(torch.sum((result-expected_result).eq(0)).item())
-    print(result)
-
-    result=torch.argmin(dtw_matrix[1], dim=1)
-    print(dtw_matrix[1])
-    print(torch.sum((result-expected_result).eq(0)).item())
-    print(result)
-
-    result=torch.argmin(dtw_matrix[2], dim=1)
-    print(dtw_matrix[2])
-    print(torch.sum((result-expected_result).eq(0)).item())
-    print(result)
-
-    result=torch.argmin(dtw_matrix[3], dim=1)
-    print(dtw_matrix[3])
-    print(torch.sum((result-expected_result).eq(0)).item())
-    print(result)
-
-    # Determine threshold
-    threshold = dtw_matrix.mean().item()
-
-    # Classify and plot confusion matrix
-    # dtw.classify_and_plot_confusion_matrix(
-    #     dtw_matrix=dtw_matrix,
-    #     training_labels=training_set['labels'],
-    #     class_labels=class_repr['labels'],
-    #     threshold=threshold
-    # )
-
-    print("Done, no errors")
-
-
 
 
